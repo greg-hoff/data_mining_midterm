72ac9021219e454d03bd3f319ae63138064174bd
greg-hoff <gjh4@njit.edu>
2025-09-25 20:48:42 -0400

    aprirori alg to test support/conf

---
+++ apriori.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+
+data = pd.read_csv('trans1_onehot.csv')
+pd.set_option('display.max_rows', None)  # Show all rows
+data = data.astype(bool)  # Convert 0/1 to True/False
+print(data)
+
+# Find frequent itemsets with a minimum support of 0.1
+frequent_itemsets = apriori(data, min_support=0.1, use_colnames=True)
+print(frequent_itemsets)
+
+# Generate rules with a minimum confidence of 0.5
+rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.5)
+print(rules[['antecedents', 'consequents', 'antecedent support', 'consequent support', 'support', 'confidence']])
